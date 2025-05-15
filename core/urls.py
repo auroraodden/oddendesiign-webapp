39a2ef58e3fcdbb26f249c2c61e6cbc01e4c41f3
Aurora Odden <auroraodden@gmail.com>
2025-05-16 01:32:19 +0200

    Add outlet view and template

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('gallery/', views.gallery, name='gallery'),
+    path('outlet/', views.outlet, name='outlet'), # Kobler URL-en /outlet/ til funksjonen outlet(), da vet Django hvilken visning den skal kjøre når noen besøker den adressen
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,9 @@ from .models import GalleryImage
 def gallery(request):
     images = GalleryImage.objects.all()
     return render(request, 'core/gallery.html', {'images': images})
+
+from .models import Product
+
+def outlet(request):
+    products = Product.objects.filter(is_available=True) # Henter kun produkter som er tilgjengelige for kjøp, ikke utgåtte produkter
+    return render(request, 'core/outlet.html', {'products': products}) # Sender listen med produkter videre til en egen html-fil
