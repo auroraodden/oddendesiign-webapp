afe94ffb3d19565326d07ad95a7b43687ccdbc6d
Aurora Odden <auroraodden@Aurora-sin-MacBook-Pro.local>
2025-05-15 14:05:17 +0200

    Add models for customer, product, order, review and gallery

--- Oddendesiign_Webapp/settings.py
+++ Oddendesiign_Webapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'core', #Registrerer core-appen i Django, slik at Django vet at appen eksisterer
 ]
 
 MIDDLEWARE = [
--- core/models.py
+++ core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    group_name = models.CharField(max_length=255)
+    contact_email = models.EmailField() # Obligatorisk
+    contact_phone =models.CharField(max_length=20) # Obligatorisk
+    address = models.TextField() # Obligatorisk
+
+    def __str__(self):
+        return self.group_name 
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/', blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    custom_description = models.TextField(blank=True) # Beskrivelse av produktet
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer.group_name}"
+
+class Review(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    text = models.TextField()
+    rating = models.IntegerField(default=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.customer}"
+
+class GalleryImage(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='gallery/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
