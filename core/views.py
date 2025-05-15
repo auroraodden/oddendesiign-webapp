a138fdf2b4ed477524cb2d403c281b6097b97db6
Aurora Odden <auroraodden@Aurora-sin-MacBook-Pro.local>
2025-05-15 22:21:38 +0200

    Add gallery view and template

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+from .models import GalleryImage
+
+def gallery(request):
+    images = GalleryImage.objects.all()
+    return render(request, 'core/gallery.html', {'images': images})
