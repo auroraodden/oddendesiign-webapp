abca250674388aec6dd4389180eb5b23a9118fc6
Aurora Odden <auroraodden@gmail.com>
2025-05-19 14:53:27 +0200

    Add success page after order submission

---
+++ core/forms.py
@@ -0,0 +1,58 @@
+from django import forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = [
+            "product",
+            "group_name",
+            "concept_description",
+            "uploaded_file",
+            "full_name",
+            "email",
+            "phone",
+            "address",
+            "postal_code",
+            "city",
+            "birth_date",
+            "color_1",
+            "color_2",
+            "color_3",
+            "color_4",
+            "color_5",
+            "agree_to_terms",
+            "total_price"
+        ]
+
+        labels ={
+            "product": "Vennligst velg produkt",
+            "group_name": "Russenavn",
+            "concept_description": "Beskriv ditt konsept",
+            "uploaded_file": "Last opp fil (valgfritt)",
+            "full_name": "Fullt navn",
+            "email": "E-post",
+            "phone": "Telefonnummer",
+            "address": "Adresse",
+            "postal_code": "Postnummer",
+            "city": "By",
+            "birth_date": "Fødselsdato",
+            "color_1": "Farge 1",
+            "color_2": "Farge 2",
+            "color_3": "Farge 3",
+            "color_4": "Farge 4",
+            "color_5": "Farge 5",
+            "agree_to_terms": "Jeg godkjenner betingelsene og vilkårene",
+            "total_price": "Total sum (NOK)",
+        }
+
+        widgets = {
+            "birth_date": forms.DateInput(attrs={"type": "date"}),
+            "color_1": forms.TextInput(attrs={"type": "color"}),
+            "color_2": forms.TextInput(attrs={"type": "color"}),
+            "color_3": forms.TextInput(attrs={"type": "color"}),
+            "color_4": forms.TextInput(attrs={"type": "color"}),
+            "color_5": forms.TextInput(attrs={"type": "color"}),
+            "agree_to_terms": forms.CheckboxInput(),
+            "total_price": forms.NumberInput(attrs={"readonly": "readonly"}),
+        }
--- core/views.py
+++ core/views.py
@@ -20,3 +20,16 @@ from .models import Review
 def reviews_page(request):
     reviews = Review.objects.all() # Henter alle anmeldelser fra databasen
     return render(request, 'core/reviews.html', {'reviews': reviews}) # Sender listen med anmeldelser videre til en egen html-fil (reviews.html)
+
+from .forms import OrderForm
+
+def order_view(request):
+    if request.method == 'POST': # Hvis skjemaet ble sendt inn, validerers det og lagres i databasen
+        form = OrderForm(request.POST, request.FILES) 
+        if form.is_valid():
+            form.save()
+            return render(request, 'core/order_success.html')
+    else:
+        form = OrderForm()
+
+    return render(request, 'core/order.html', {'form': form}) # Template-filen som skal vise skjemaet
