26e5bc03fc609b03956fcf16c397fa81aa3c6267
Aurora Odden <auroraodden@gmail.com>
2025-05-19 14:20:45 +0200

    Add URL route for order form

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('gallery/', views.gallery, name='gallery'),
     path('outlet/', views.outlet, name='outlet'), # Kobler til /outlet/-funksjonen i views.py
     path('reviews/', views.reviews_page, name='reviews'), # Kobler til /reviews/-funksjonen i views.py
+    path('order/', views.order_view, name='order'), # Kobler til /order/-funksjonen i views.py
 ]
